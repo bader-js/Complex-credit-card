acc3aabe7c64b1719c2c395703fedd8e3621fd4c
Mohamed Bader Ben Rejeb <sky.bader@gmail.com>
2019-11-15 09:31:07 +0100

    hihi

---
+++ src/components/cards.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default class SupportedCards extends React.Component {
+  render() {
+    return (
+      <div className="App-cards">
+        <h3>Supported Cards</h3>
+        <div className="App-cards-list">
+          
+        </div>
+      </div>
+    );
+  }
+}
